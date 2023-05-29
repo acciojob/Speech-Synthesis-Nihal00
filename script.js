6972de512e5e504620ba87a185e5fa69aa5b5a11
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-29 12:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your script here.
+const text = document.querySelector('textarea');
+const start = document.getElementById('speak');
+const stop = document.getElementById('stop');
+
+let utterance = new SpeechSynthesisUtterance(text.value);
+
+start.addEventListener('click', () => {
+	window.speechSynthesis.speak(utterance); 
+});
+
+stop.addEventListener('click', () => {
+	window.speechSynthesis.cancel()
+});
